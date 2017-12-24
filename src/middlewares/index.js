8aadc32fa457f38c157fb43665ccbff09bfe8ab8
yalap_jeanclaude <yalap.jeanclaude@gmail.com>
2017-12-24 15:39:13 +0100

    Remove koa-passport module

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,7 +2,6 @@ import compress from 'koa-compress';
 import helmet from 'koa-helmet';
 import bodyParser from 'koa-bodyparser';
 import cors from 'kcors';
-import passport from 'koa-passport';
 import { RateLimit, Stores as RateLimitStores } from 'koa2-ratelimit';
 
 import I18n from './I18n';
@@ -11,7 +10,6 @@ import handleError from './handleError';
 import logger from './logger';
 
 module.exports = {
-  passport,
   cors,
   helmet,
   bodyParser,
