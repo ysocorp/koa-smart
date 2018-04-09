311763fd2d701ce260fd2cd718e4791cc46fbd6b
yalap_jeanclaude <yalap.jeanclaude@gmail.com>
2018-04-09 11:34:56 +0200

    VERSION 1.2.2

--- dist/middlewares/addDefaultBody.js
+++ dist/middlewares/addDefaultBody.js
@@ -12,13 +12,17 @@ var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
+var _stringify = require("babel-runtime/core-js/json/stringify");
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
 var addDefaultBody = function () {
   var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(ctx, next) {
     return _regenerator2.default.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            ctx.body = ctx.body || options.body;
+            ctx.body = ctx.body || copyBody();
             _context.next = 3;
             return next();
 
@@ -40,6 +44,10 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var options = {
   body: {}
 };
+
+function copyBody() {
+  return JSON.parse((0, _stringify2.default)(options.body));
+}
 
 exports.default = function () {
   var body = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
